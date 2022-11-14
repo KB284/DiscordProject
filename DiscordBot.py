26bf58c305a0fdb94ba91fa4233578788cc9da1a
KB284 <kfarah5030@eagle.fgcu.edu>
2022-11-13 20:08:34 -0500

    Update to add different TOKEN

--- DiscordBot.py
+++ DiscordBot.py
@@ -6,21 +6,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('Project')
 
 client = discord.Client(intents=discord.Intents.default())
 
 @client.event
 async def on_ready():
+    print(f'{client.user.name} has connected to Discord!')
 
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
-
-    print(f'{client.user} is connected to the following guild:\n')
-    print(f'{guild.name}(id: {guild.id})')
-
-    members =  '\n -'.join([member.name for member in guild.members])
-    print(f'Guild Members:\n - {members}')
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
 
 client.run(TOKEN)
